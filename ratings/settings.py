f64be6754aea8f7f5a26d49783a0609f44a39ca9
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-08 19:43:42 +0300

    configured Postgres DB

--- ratings/settings.py
+++ ratings/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'ratings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ratings',
+        'USER': 'user',
+        'PASSWORD':'Mzazi25',
     }
 }
+
 
 
 # Password validation
