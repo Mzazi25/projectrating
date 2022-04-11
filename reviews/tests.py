08bcf2e17dc477d15c0f3d2f4fb1165ae865810d
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-11 08:51:33 +0300

    Added tests

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Project,Profile,Rating
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    # testing project
+    def setUp(self):
+        self.project= Project(name = 'James', owner ='John', link = "www.john.com",description = "john",pub_date ="2020")
+    # Testing  profile
+    def setUp(self):
+        self.profile= Profile(username="Mzazi", description = "student of life", contact ="0188381" )
+        # Testing Rating
+    def setUp(self):
+        self.rating= Rating(design = 'James', usability ='John', content = "Johny",ratings = "john", project = "Delani Studio", published_date ="2020")
+    
+    # Testing Save Method
+    def test_save_method(self):
+        self.project.save()
+        self.profile.save()
+        self.rating.save()
+
+    def tearDown(self):        
+        Project.objects.all().delete()
+        Profile.objects.all().delete()
+        Rating.objects.all().delete()
+
+        
